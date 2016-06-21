c58d48dccca6e8a8af34769dd10e34abc398ed86
Rudolf Bargholz <rudolf@bargholz.ch>
2016-06-21 09:59:27 +0200

    Error with OneTime DIFF Import
    Additional cosmetic changes

--- sql/import-diff/import-diff-linux.sql
+++ sql/import-diff/import-diff-linux.sql
@@ -1,8 +1,8 @@
 
 -- REPLACE löscht alle Daten in der Tabelle vor dem Import
-IMPORT FROM ../import/tooHotels.csv    OF DEL MODIFIED BY norowwarnings METHOD P ( 1, 33 ) COMMITCOUNT 10000  REPLACE INTO TOOHOTEL2 ( HOTELKEY, TOCODE ) @
-IMPORT FROM ../import/tooRooms.csv     OF DEL                           METHOD P ( 2, 29 ) COMMITCOUNT 10000  REPLACE INTO TOOROOMS2 ( ROOMKEY, TOCODE ) @
-IMPORT FROM ../import/tooMisc.csv      OF DEL                           METHOD P ( 1, 2 )  COMMITCOUNT 10000  REPLACE INTO TOOMISC2  ( MISCKEY, TOCODE ) @
+IMPORT FROM ../import_diff/tooHotels.csv    OF DEL MODIFIED BY norowwarnings METHOD P ( 1, 33 ) COMMITCOUNT 10000  REPLACE INTO TOOHOTEL2 ( HOTELKEY, TOCODE ) @
+IMPORT FROM ../import_diff/tooRooms.csv     OF DEL                           METHOD P ( 2, 29 ) COMMITCOUNT 10000  REPLACE INTO TOOROOMS2 ( ROOMKEY, TOCODE ) @
+IMPORT FROM ../import_diff/tooMisc.csv      OF DEL                           METHOD P ( 1, 2 )  COMMITCOUNT 10000  REPLACE INTO TOOMISC2  ( MISCKEY, TOCODE ) @
 
 
 delete from TOOHOTEL where (TOOHOTEL.HOTELKEY, TOOHOTEL.TOCODE)                           in (select TOOHOTEL2.HOTELKEY, TOOHOTEL2.TOCODE from TOOHOTEL2) @
@@ -26,13 +26,13 @@ delete from TOOADDPERIODPRICE where (TOOADDPERIODPRICE.ITEMKEY, TOOADDPERIODPRIC
 delete from TOODESCRIPTIONS where (TOODESCRIPTIONS.ITEMKEY, TOODESCRIPTIONS.TOCODE)       in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOODESCRIPTIONS.ITEMTYPE   ='M' @
 delete from TOOPERDAYPRICE where (TOOPERDAYPRICE.ITEMKEY, TOOPERDAYPRICE.TOCODE)          in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOPERDAYPRICE.ITEMTYPE    ='M' @
 delete from TOOADDPERDAYPRICE where (TOOADDPERDAYPRICE.ITEMKEY, TOOADDPERDAYPRICE.TOCODE) in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOADDPERDAYPRICE.ITEMTYPE ='M' @
-delete from TOOONETIME where (TOOONETIME.ITEMKEY, TOOONETIME.TOCODE)                      in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOONETIME.ITEMTYPE        ='H' @
+delete from TOOONETIME where (TOOONETIME.ITEMKEY, TOOONETIME.TOCODE)                      in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOONETIME.ITEMTYPE        ='M' @
 delete from TOOSPECIALOFFERS where (TOOSPECIALOFFERS.ITEMKEY, TOOSPECIALOFFERS.TOCODE)    in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOSPECIALOFFERS.ITEMTYPE  ='M' @
 delete from TOOEARLYBOOKINGS where (TOOEARLYBOOKINGS.ITEMKEY, TOOEARLYBOOKINGS.TOCODE)    in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOEARLYBOOKINGS.ITEMTYPE  ='M' @
 delete from TOOALLOTMENTS where (TOOALLOTMENTS.ITEMKEY, TOOALLOTMENTS.TOCODE)             in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOALLOTMENTS.ITEMTYPE     ='M' @
 delete from TOOCANCELLATIONS where (TOOCANCELLATIONS.ITEMKEY, TOOCANCELLATIONS.TOCODE)    in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOCANCELLATIONS.ITEMTYPE  ='M' @
 delete from TOOITEMINFOS where (TOOITEMINFOS.ITEMKEY, TOOITEMINFOS.TOCODE)                in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOITEMINFOS.ITEMTYPE      ='M' @
-delete from TOOPERIODPRICE where (TOOPERIODPRICE.ITEMKEY, TOOPERIODPRICE.TOCODE)          in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)     and TOOPERIODPRICE.ITEMTYPE     ='M' @
+delete from TOOPERIODPRICE where (TOOPERIODPRICE.ITEMKEY, TOOPERIODPRICE.TOCODE)          in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOPERIODPRICE.ITEMTYPE    ='M' @
 delete from TOOADDPERIODPRICE where (TOOADDPERIODPRICE.ITEMKEY, TOOADDPERIODPRICE.TOCODE) in (select TOOMISC2.MISCKEY, TOOMISC2.TOCODE from TOOMISC2)      and TOOADDPERIODPRICE.ITEMTYPE ='M' @
 
 
@@ -53,22 +53,22 @@ alter table TOOMISCTEXT         alter ID set cache 20 @
 
 -- INSERT fügt nur die neuen Daten hinzu. Da die Daten oben gelöscht wurden führt dies zu keinen Konflikten.
 -- Am Besten wäre wenn der DELETE in einer Transaktion wäre, und bei einem Problem gibt es ein ROLLBACK.
-IMPORT FROM ../import/tooRooms.csv           OF DEL                                              COMMITCOUNT 10000   INSERT INTO TOOROOMS @
-IMPORT FROM ../import/tooMisc.csv            OF DEL                                              COMMITCOUNT 10000   INSERT INTO TOOMISC @
-IMPORT FROM ../import/tooMiscHotel.csv       OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOMISCHOTEL @
-IMPORT FROM ../import/tooMiscText.csv        OF DEL MODIFIED BY identitymissing delprioritychar  COMMITCOUNT 10000   INSERT INTO TOOMISCTEXT @
-IMPORT FROM ../import/tooDescriptions.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOODESCRIPTIONS @
-IMPORT FROM ../import/tooPerDayPrices.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOPERDAYPRICE @
-IMPORT FROM ../import/tooAddPerDayPrices.csv OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOADDPERDAYPRICE @
-IMPORT FROM ../import/tooOneTimes.csv        OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOONETIME @
-IMPORT FROM ../import/tooSpecialOffers.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOSPECIALOFFERS @
-IMPORT FROM ../import/tooEarlyBookings.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOEARLYBOOKINGS @
-IMPORT FROM ../import/tooAllotments.csv      OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOALLOTMENTS @
-IMPORT FROM ../import/tooCancellations.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOCANCELLATIONS @
-IMPORT FROM ../import/tooItemInfos.csv       OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOITEMINFOS @
-IMPORT FROM ../import/tooPeriodPrices.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOPERIODPRICE @
-IMPORT FROM ../import/tooAddPeriodPrices.csv OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOADDPERIODPRICE @
-IMPORT FROM ../import/tooHotels.csv          OF DEL MODIFIED BY norowwarnings                    COMMITCOUNT 10000   INSERT INTO TOOHOTEL @
+IMPORT FROM ../import_diff/tooRooms.csv           OF DEL                                              COMMITCOUNT 10000   INSERT INTO TOOROOMS @
+IMPORT FROM ../import_diff/tooMisc.csv            OF DEL                                              COMMITCOUNT 10000   INSERT INTO TOOMISC @
+IMPORT FROM ../import_diff/tooMiscHotel.csv       OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOMISCHOTEL @
+IMPORT FROM ../import_diff/tooMiscText.csv        OF DEL MODIFIED BY identitymissing delprioritychar  COMMITCOUNT 10000   INSERT INTO TOOMISCTEXT @
+IMPORT FROM ../import_diff/tooDescriptions.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOODESCRIPTIONS @
+IMPORT FROM ../import_diff/tooPerDayPrices.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOPERDAYPRICE @
+IMPORT FROM ../import_diff/tooAddPerDayPrices.csv OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOADDPERDAYPRICE @
+IMPORT FROM ../import_diff/tooOneTimes.csv        OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOONETIME @
+IMPORT FROM ../import_diff/tooSpecialOffers.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOSPECIALOFFERS @
+IMPORT FROM ../import_diff/tooEarlyBookings.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOEARLYBOOKINGS @
+IMPORT FROM ../import_diff/tooAllotments.csv      OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOALLOTMENTS @
+IMPORT FROM ../import_diff/tooCancellations.csv   OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOCANCELLATIONS @
+IMPORT FROM ../import_diff/tooItemInfos.csv       OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOITEMINFOS @
+IMPORT FROM ../import_diff/tooPeriodPrices.csv    OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOPERIODPRICE @
+IMPORT FROM ../import_diff/tooAddPeriodPrices.csv OF DEL MODIFIED BY identitymissing                  COMMITCOUNT 10000   INSERT INTO TOOADDPERIODPRICE @
+IMPORT FROM ../import_diff/tooHotels.csv          OF DEL MODIFIED BY norowwarnings                    COMMITCOUNT 10000   INSERT INTO TOOHOTEL @
 
 
 alter table TOODESCRIPTIONS      alter ID set no cache @
