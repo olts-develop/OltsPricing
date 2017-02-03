cde0f41b9311cbf1f81d46f4c620c882e5d1d3d9
Rudolf Bargholz <rudolf@bargholz.ch>
2017-02-03 18:53:05 +0100

    no misc prices for children
    Take into account cases where there are no misc prices for children. In
    this case the child is taken to be an adult.

--- sql/functions/func_miscvalid.sql
+++ sql/functions/func_miscvalid.sql
@@ -1,4 +1,4 @@
--- -----------------------------------------------------------------------------
+﻿-- -----------------------------------------------------------------------------
 -- Function name: *func_miscvalid*
 
 -- Returns a either a row with the nr adults and birth dates for the children
@@ -41,6 +41,15 @@ BEGIN
 	DECLARE totalpaxnr INTEGER;
 	DECLARE enddate DATE;
 	DECLARE duration INTEGER;
+	DECLARE child1_hasprice INTEGER;
+	DECLARE child2_hasprice INTEGER;
+	DECLARE child3_hasprice INTEGER;
+	DECLARE child4_hasprice INTEGER;
+	DECLARE nradults INTEGER;
+	DECLARE childbirthdate1 DATE;
+	DECLARE childbirthdate2 DATE;
+	DECLARE childbirthdate3 DATE;
+	DECLARE childbirthdate4 DATE;
 
 	SET childage1 = ((DAYS(p_startdate) - DAYS(p_childbirthdate1)) / 365);
 	SET childage2 = ((DAYS(p_startdate) - DAYS(p_childbirthdate2)) / 365);
@@ -77,33 +86,357 @@ BEGIN
 			);
 	SET enddate = coalesce(p_enddate, p_startdate);
 	SET duration = DAYS(p_enddate) - DAYS(p_startdate) + 1;
+	SET child1_hasprice = (
+			CASE 
+				WHEN childage1 < 2
+					THEN 1
+				WHEN (
+						SELECT count(ID)
+						FROM TOOADDPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage1
+							AND AGETO >= childage1
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOADDPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage1
+							AND AGETO >= childage1
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOONETIME
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage1
+							AND AGETO >= childage1
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage1
+							AND AGETO >= childage1
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage1
+							AND AGETO >= childage1
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					THEN 1
+				ELSE 0
+				END
+			);
+	SET child2_hasprice = (
+			CASE 
+				WHEN childage2 < 2
+					THEN 1
+				WHEN (
+						SELECT count(ID)
+						FROM TOOADDPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage2
+							AND AGETO >= childage2
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOADDPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage2
+							AND AGETO >= childage2
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOONETIME
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage2
+							AND AGETO >= childage2
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage2
+							AND AGETO >= childage2
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage2
+							AND AGETO >= childage2
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					THEN 1
+				ELSE 0
+				END
+			);
+	SET child3_hasprice = (
+			CASE 
+				WHEN childage3 < 2
+					THEN 1
+				WHEN (
+						SELECT count(ID)
+						FROM TOOADDPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage3
+							AND AGETO >= childage3
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOADDPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage3
+							AND AGETO >= childage3
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOONETIME
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage3
+							AND AGETO >= childage3
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage3
+							AND AGETO >= childage3
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage3
+							AND AGETO >= childage3
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					THEN 1
+				ELSE 0
+				END
+			);
+	SET child4_hasprice = (
+			CASE 
+				WHEN childage4 < 2
+					THEN 1
+				WHEN (
+						SELECT count(ID)
+						FROM TOOADDPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND TOOADDPERDAYPRICE.DAY = p_startdate
+							AND AGEFROM <= childage4
+							AND AGETO >= childage4
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOADDPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage4
+							AND AGETO >= childage4
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOONETIME
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage4
+							AND AGETO >= childage4
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERDAYPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DAY = p_startdate
+							AND AGEFROM <= childage4
+							AND AGETO >= childage4
+							AND CHILDIDXNR > 0 FETCH first 1 row ONLY
+						) = 1
+					OR (
+						SELECT count(ID)
+						FROM TOOPERIODPRICE
+						WHERE ITEMKEY = p_misckey
+							AND ITEMTYPE = 'M'
+							AND TOCODE = p_tocode
+							AND DATEFROM <= p_startdate
+							AND DATETO >= p_startdate
+							AND AGEFROM <= childage4
+							AND AGETO >= childage4
+							AND CHILD = 1 FETCH first 1 row ONLY
+						) = 1
+					THEN 1
+				ELSE 0
+				END
+			);
+	SET nradults = p_nradults + (
+			CASE 
+				WHEN p_childbirthdate1 IS NULL
+					THEN 0
+				WHEN child1_hasprice = 1
+					THEN 0
+				ELSE 1
+				END
+			) + (
+			CASE 
+				WHEN p_childbirthdate2 IS NULL
+					THEN 0
+				WHEN child2_hasprice = 1
+					THEN 0
+				ELSE 1
+				END
+			) + (
+			CASE 
+				WHEN p_childbirthdate3 IS NULL
+					THEN 0
+				WHEN child3_hasprice = 1
+					THEN 0
+				ELSE 1
+				END
+			) + (
+			CASE 
+				WHEN p_childbirthdate4 IS NULL
+					THEN 0
+				WHEN child4_hasprice = 1
+					THEN 0
+				ELSE 1
+				END
+			);
+	SET childbirthdate1 = (
+			CASE 
+				WHEN child1_hasprice = 1
+					THEN p_childbirthdate1
+				ELSE cast(NULL AS DATE)
+				END
+			);
+	SET childbirthdate2 = (
+			CASE 
+				WHEN child2_hasprice = 1
+					THEN p_childbirthdate2
+				ELSE cast(NULL AS DATE)
+				END
+			);
+	SET childbirthdate3 = (
+			CASE 
+				WHEN child3_hasprice = 1
+					THEN p_childbirthdate3
+				ELSE cast(NULL AS DATE)
+				END
+			);
+	SET childbirthdate4 = (
+			CASE 
+				WHEN child4_hasprice = 1
+					THEN p_childbirthdate4
+				ELSE cast(NULL AS DATE)
+				END
+			);
 
 	RETURN
 
-	SELECT
-		p_nradults
-		,func_getposdatedesc(0, 1, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)
-		,func_getposdatedesc(0, 2, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)
-		,func_getposdatedesc(0, 3, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)
-		,func_getposdatedesc(0, 4, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)
+	SELECT nradults
+		,func_getposdatedesc(0, 1, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4)
+		,func_getposdatedesc(0, 2, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4)
+		,func_getposdatedesc(0, 3, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4)
+		,func_getposdatedesc(0, 4, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4)
 		,(
-			CASE
+			CASE 
 				WHEN minimumdays > 0
-					AND DAYS(enddate) - DAYS(p_startdate) < minimumdays -1
+					AND DAYS(enddate) - DAYS(p_startdate) < minimumdays - 1
 					THEN p_startdate + (minimumdays) DAYS
 				WHEN minimumdays > 0
-					AND DAYS(enddate) - DAYS(p_startdate) > maximumdays -1
+					AND DAYS(enddate) - DAYS(p_startdate) > maximumdays - 1
 					THEN p_startdate + (maximumdays) DAYS
 				ELSE enddate + 1 DAY
 				END
-			) as PRICEENDDATE
+			) AS PRICEENDDATE
 		,(
 			CASE 
 				WHEN startdaterelevant = 1
 					THEN p_startdate + 1 DAY
 				ELSE enddate + 1 DAY
 				END
-			) as ALLOTMENTENDDATE
+			) AS ALLOTMENTENDDATE
 	FROM toomisc
 	WHERE misckey = p_misckey
 		AND tocode = p_tocode
@@ -117,7 +450,7 @@ BEGIN
 				AND maximumpersons >= totalpaxnr
 				)
 			)
-		AND p_nradults > 0
+		AND nradults > 0
 		AND minimumdays <= maximumdays;
 END
 @
