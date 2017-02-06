09f5f324ef8e51b88da2b983633afa84984b4855
Rudolf Bargholz <rudolf@bargholz.ch>
2017-02-06 14:22:05 +0100

    Added func_miscpriceav2_tbl
    Added func_miscpriceav2_tbl . This returns the search in a tabular
    format, with the pricing in a detailed format.

--- sql/functions/func_miscpricebydest.sql
+++ sql/functions/func_miscpricebydest.sql
@@ -433,3 +433,246 @@ BEGIN
       );
 END
 @
+
+DROP FUNCTION func_miscpriceav2_tbl @
+
+--   SELECT TOOMISC.TOCODE AS TOCODE
+--     ,TOOMISC.MISCKEY AS MISCKEY
+--     ,TOOMISC.DESTINATIONCODE AS DESTINATIONCODE
+--     ,TOOMISC.MISCCODE AS MISCCODE
+--     ,TOOMISC.MISCITEMCODE AS MISCITEMCODE
+--     ,NR
+--     ,PRICE
+--     ,TOTAL
+--     ,TYPE1
+--     ,TYPE2
+--     ,FROMDATE
+--     ,TODATE
+--     ,x.DESCID
+--     ,P_SEQ
+--     ,STATUS
+--     ,TOODESCRIPTIONS.ID
+--     ,DESCDE
+--     ,DESCEN
+--     ,DESCFR
+--     ,DESCIT
+--   FROM TOOMISC
+--   INNER JOIN TABLE (func_miscpriceav2_tbl('', '', '2017-04-01', '2017-04-01', 3, '', '', '', '', '', '', 1, 0, 1, '', 'TOUWAL10010')) AS x ON x.TOCODE = TOOMISC.TOCODE
+--     AND x.MISCKEY = TOOMISC.MISCKEY
+--   LEFT OUTER JOIN TOODESCRIPTIONS ON TOODESCRIPTIONS.ITEMKEY = x.MISCKEY
+--     AND ITEMTYPE = 'M'
+--     AND TOODESCRIPTIONS.TOCODE = ''
+--     AND TOODESCRIPTIONS.DESCID = x.DESCID
+--   ORDER BY x.TOCODE
+--     ,x.MISCKEY
+--     ,(
+--       CASE x.TYPE1
+--         WHEN 'PP'
+--           THEN 1
+--         WHEN 'APP'
+--           THEN 2
+--         WHEN 'PDP'
+--           THEN 3
+--         WHEN 'APDP'
+--           THEN 4
+--         WHEN 'OT'
+--           THEN 5
+--         WHEN 'SO'
+--           THEN 6
+--         WHEN 'EB'
+--           THEN 7
+--         ELSE 8
+--         END
+--       ) ASC
+--     ,x.TYPE2 ASC
+--     ,x.FROMDATE ASC
+--
+-- TOCODE  MISCKEY      DESTINATIONCODE  MISCCODE  MISCITEMCODE  NR  PRICE  TOTAL  TYPE1  TYPE2  FROMDATE    TODATE      DESCID  P_SEQ     STATUS  ID       DESCDE                DESCEN               DESCFR                    DESCIT
+--         TOUWAL10010  BKK              TRAABL    3             3   30.00  90.00  PDP    ADT    2017-04-01  2017-04-01  1       12040840  99      2173902  Preis pro Person/Weg  rate per person/way  prix par personne/trajet  rate per person/way
+
+
+CREATE FUNCTION func_miscpriceav2_tbl (
+  IN_TOCODE VARCHAR(5) DEFAULT ''
+  ,IN_DESTINATIONCODE VARCHAR(5) DEFAULT ''
+  ,IN_PRICEDATEFROM VARCHAR(10)
+  ,IN_PRICEDATETO VARCHAR(10)
+  ,IN_NRADULTS INTEGER DEFAULT 2
+  ,IN_MISCCODE VARCHAR(30) DEFAULT ''
+  ,IN_MISCITEMCODE VARCHAR(20) DEFAULT ''
+  ,IN_CHDDOB1 VARCHAR(10) DEFAULT ''
+  ,IN_CHDDOB2 VARCHAR(10) DEFAULT ''
+  ,IN_CHDDOB3 VARCHAR(10) DEFAULT ''
+  ,IN_CHDDOB4 VARCHAR(10) DEFAULT ''
+  ,IN_IGNORE_XX INTEGER DEFAULT 0
+  ,IN_IGNORE_RQ INTEGER DEFAULT 0
+  ,IN_IGNORE_PRICE0 INTEGER DEFAULT 1
+  ,IN_CURRENTDATE VARCHAR(10) DEFAULT ''
+  ,IN_MISCKEY VARCHAR(20) DEFAULT ''
+  )
+RETURNS TABLE (
+  TOCODE VARCHAR(5)
+  ,MISCKEY VARCHAR(20)
+  ,NR INTEGER
+  ,PRICE DECIMAL(10, 2)
+  ,TOTAL DECIMAL(10, 2)
+  ,TYPE1 VARCHAR(20)
+  ,TYPE2 VARCHAR(20)
+  ,FROMDATE DATE
+  ,TODATE DATE
+  ,DESCID INTEGER
+  ,P_SEQ VARCHAR(20)
+  ,STATUS VARCHAR(2)
+  ) NOT DETERMINISTIC LANGUAGE SQL
+
+BEGIN
+  ATOMIC
+
+  DECLARE startdate DATE;
+  DECLARE enddate DATE;
+  DECLARE currentdate DATE;
+  DECLARE p_childbirthdate1 DATE;
+  DECLARE p_childbirthdate2 DATE;
+  DECLARE p_childbirthdate3 DATE;
+  DECLARE p_childbirthdate4 DATE;
+
+  SET startdate = cast(nullif(IN_PRICEDATEFROM, '') AS DATE);
+  SET enddate = cast(nullif(IN_PRICEDATETO, '') AS DATE);
+  SET currentdate = coalesce(cast(nullif(IN_CURRENTDATE, '') AS DATE), CURRENT DATE);
+  SET p_childbirthdate1 = cast(nullif(IN_CHDDOB1, '') AS DATE);
+  SET p_childbirthdate2 = cast(nullif(IN_CHDDOB2, '') AS DATE);
+  SET p_childbirthdate3 = cast(nullif(IN_CHDDOB3, '') AS DATE);
+  SET p_childbirthdate4 = cast(nullif(IN_CHDDOB4, '') AS DATE);
+
+  RETURN
+  WITH tmptable1(MISCKEY, NRADULTS, CHDDOB1, CHDDOB2, CHDDOB3, CHDDOB4, PRICEENDDATE, ALLOTMENTENDDATE) AS (
+      SELECT TOOMISC.MISCKEY
+        ,x.NRADULTS
+        ,x.CHILDBIRTHDATE1
+        ,x.CHILDBIRTHDATE2
+        ,x.CHILDBIRTHDATE3
+        ,x.CHILDBIRTHDATE4
+        ,x.PRICEENDDATE
+        ,x.ALLOTMENTENDDATE
+      FROM TOOMISC TOOMISC
+        ,TABLE (func_miscvalid(IN_TOCODE, TOOMISC.MISCKEY, startdate, enddate, IN_NRADULTS, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)) AS x
+      WHERE TOOMISC.DESTINATIONCODE = coalesce(nullif(IN_DESTINATIONCODE, ''), TOOMISC.DESTINATIONCODE)
+        AND TOOMISC.MISCCODE = coalesce(nullif(IN_MISCCODE, ''), TOOMISC.MISCCODE)
+        AND TOOMISC.MISCITEMCODE = coalesce(nullif(IN_MISCITEMCODE, ''), TOOMISC.MISCITEMCODE)
+        AND TOOMISC.MISCKEY = coalesce(nullif(IN_MISCKEY, ''), TOOMISC.MISCKEY)
+      )
+    ,tmptable2(MISCKEY, NRADULTS, CHDDOB1, CHDDOB2, CHDDOB3, CHDDOB4, PRICEENDDATE, STATUS) AS (
+      SELECT x.MISCKEY
+        ,x.NRADULTS
+        ,x.CHDDOB1
+        ,x.CHDDOB2
+        ,x.CHDDOB3
+        ,x.CHDDOB4
+        ,x.PRICEENDDATE
+        ,(
+          CASE 
+            WHEN (
+                coalesce(TOOMISC.PASSIVE, 0) = 0
+                OR (
+                  TOOMISC.PASSIVE = 1
+                  AND coalesce(TOOMISC.PASSIVEFROMDATE, currentdate) > currentdate
+                  )
+                )
+              THEN func_get_allotment2(IN_TOCODE, x.MISCKEY, 'M', enddate, x.ALLOTMENTENDDATE, currentdate)
+            ELSE 'XX'
+            END
+          )
+      FROM tmptable1 x
+        ,TOOMISC TOOMISC
+      WHERE TOOMISC.MISCKEY = x.MISCKEY
+      )
+    ,tmptable3(MISCKEY, STATUS, TOTAL) AS (
+      SELECT max(x.MISCKEY) AS MISCKEY
+        ,max(x.STATUS) AS STATUS
+        ,sum(cast(pricing.TOTAL AS FLOAT)) AS TOTAL
+      FROM tmptable2 x
+        ,TABLE (func_pricing2_tbl(IN_TOCODE, x.MISCKEY, 'M', startdate, x.PRICEENDDATE, currentdate, x.NRADULTS, x.CHDDOB1, x.CHDDOB2, x.CHDDOB3, x.CHDDOB4)) AS pricing
+      GROUP BY x.MISCKEY
+      )
+    ,tmptable4(MISCKEY, STATUS, NR, PRICE, TOTAL, TYPE1, TYPE2, FROMDATE, TODATE, DESCID, P_SEQ) AS (
+      SELECT x.MISCKEY AS MISCKEY
+        ,x.STATUS AS STATUS
+        ,pricing.NR AS NR
+        ,pricing.PRICE AS PRICE
+        ,pricing.TOTAL AS TOTAL
+        ,pricing.TYPE1 AS TYPE1
+        ,pricing.TYPE2 AS TYPE2
+        ,pricing.FROMDATE AS FROMDATE
+        ,pricing.TODATE AS TODATE
+        ,pricing.DESCID AS DESCID
+        ,pricing.P_SEQ AS P_SEQ
+      FROM tmptable2 x
+        ,TABLE (func_pricing2_tbl(IN_TOCODE, x.MISCKEY, 'M', startdate, x.PRICEENDDATE, currentdate, x.NRADULTS, x.CHDDOB1, x.CHDDOB2, x.CHDDOB3, x.CHDDOB4)) AS pricing
+      )
+
+  SELECT TOOMISC.TOCODE AS TOCODE
+    ,TOOMISC.MISCKEY AS MISCKEY
+    ,y.NR
+    ,y.PRICE
+    ,y.TOTAL
+    ,y.TYPE1
+    ,y.TYPE2
+    ,y.FROMDATE
+    ,y.TODATE
+    ,y.DESCID
+    ,y.P_SEQ
+    ,coalesce(x.STATUS, 'XX') AS STATUS
+  FROM TOOMISC
+  LEFT OUTER JOIN tmptable3 x ON x.MISCKEY = TOOMISC.MISCKEY
+  LEFT OUTER JOIN tmptable4 y ON y.MISCKEY = TOOMISC.MISCKEY
+  WHERE TOOMISC.TOCODE = IN_TOCODE
+    AND TOOMISC.DESTINATIONCODE = coalesce(nullif(IN_DESTINATIONCODE, ''), TOOMISC.DESTINATIONCODE)
+    AND TOOMISC.MISCCODE = coalesce(nullif(IN_MISCCODE, ''), TOOMISC.MISCCODE)
+    AND TOOMISC.MISCITEMCODE = coalesce(nullif(IN_MISCITEMCODE, ''), TOOMISC.MISCITEMCODE)
+    AND TOOMISC.MISCKEY = coalesce(nullif(IN_MISCKEY, ''), TOOMISC.MISCKEY)
+    AND coalesce(x.STATUS, 'XX') <> (
+      CASE 
+        WHEN IN_IGNORE_XX = 1
+          THEN 'XX'
+        ELSE '..'
+        END
+      )
+    AND coalesce(x.STATUS, 'XX') <> (
+      CASE 
+        WHEN IN_IGNORE_RQ = 1
+          THEN 'RQ'
+        ELSE '..'
+        END
+      )
+    AND (
+      (
+        IN_IGNORE_PRICE0 = 1
+        AND cast(x.TOTAL AS FLOAT) > 0
+        )
+      OR IN_IGNORE_PRICE0 = 0
+      )
+  ORDER BY TOOMISC.TOCODE
+    ,TOOMISC.MISCKEY
+    ,(
+      CASE y.TYPE1
+        WHEN 'PP'
+          THEN 1
+        WHEN 'APP'
+          THEN 2
+        WHEN 'PDP'
+          THEN 3
+        WHEN 'APDP'
+          THEN 4
+        WHEN 'OT'
+          THEN 5
+        WHEN 'SO'
+          THEN 6
+        WHEN 'EB'
+          THEN 7
+        ELSE 8
+        END
+      ) ASC
+    ,y.TYPE2 ASC
+    ,y.FROMDATE ASC;
+END
+
+@
