97d63979467010473b6fb4ce65a0443d1939780c
Rudolf Bargholz <rudolf@bargholz.ch>
2017-02-06 08:55:05 +0100

    Revert to old, and new function
    Reverted misc pricing methods to old functions, and added the new
    function as an explicitly new function func_miscpriceav2.

--- sql/functions/func_miscpricebydest.sql
+++ sql/functions/func_miscpricebydest.sql
@@ -116,9 +116,130 @@ DROP FUNCTION func_miscpriceav @
 
 -- select * from TABLE( func_miscpriceav ('', cast('2015-11-12' as DATE), cast('2015-11-12' as DATE), current date, 1 ) )
 CREATE FUNCTION func_miscpriceav (
-	IN_TOCODE VARCHAR(5)
-	,IN_LANGCODE VARCHAR(2) DEFAULT 'DE'
-	,IN_DESTINATIONCODE VARCHAR(5)
+	p_tocode VARCHAR(5) DEFAULT ''
+	,p_startdate DATE DEFAULT NULL
+	,p_enddate DATE DEFAULT NULL
+	,p_currentdate DATE DEFAULT CURRENT DATE
+	,p_nradults INTEGER DEFAULT 0
+	,p_childbirthdate1 DATE DEFAULT NULL
+	,p_childbirthdate2 DATE DEFAULT NULL
+	,p_childbirthdate3 DATE DEFAULT NULL
+	,p_childbirthdate4 DATE DEFAULT NULL
+	)
+RETURNS TABLE (
+	TOCODE VARCHAR(20)
+	,MISCKEY VARCHAR(20)
+	,TOTAL DECIMAL(10, 2)
+	,DESTINATIONCODE VARCHAR(5)
+	,MISCCODE VARCHAR(30)
+	,TOURBOCODE VARCHAR(20)
+	,STATUS VARCHAR(4)
+	)
+
+NOT DETERMINISTIC LANGUAGE SQL
+
+BEGIN
+	ATOMIC
+
+	RETURN
+
+	SELECT p_tocode
+		,toomisc.misckey
+		,func_miscpricing(p_tocode, toomisc.misckey, p_startdate, p_enddate, p_currentdate, p_nradults, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4) AS pricetotal
+		,destinationcode
+		,misccode
+		,miscitemcode
+		,func_get_allotment2(p_tocode, toomisc.misckey, 'M', p_startdate, x.ALLOTMENTENDDATE, p_currentdate )
+	FROM toomisc
+		, TABLE (func_miscvalid(p_tocode, toomisc.misckey, p_startdate, p_enddate, p_nradults, p_childbirthdate1, p_childbirthdate2, p_childbirthdate3, p_childbirthdate4)) AS x
+	WHERE toomisc.tocode = p_tocode
+;
+END
+@
+
+DROP FUNCTION func_miscpriceav_ch @
+
+-- select * from TABLE( func_miscpriceav_ch ('', '2015-11-12', '2015-11-12', '', 1 , '', '', '', '') )
+-- select * from TABLE( func_miscpriceav_ch ('TSOL', '2015-11-12', '2015-11-12', '', 1 , '', '', '', '') )
+CREATE FUNCTION func_miscpriceav_ch (
+	p_tocode VARCHAR(5) DEFAULT ''
+	,p_startdate VARCHAR(10) DEFAULT ''
+	,p_enddate VARCHAR(10) DEFAULT ''
+	,p_currentdate VARCHAR(10) DEFAULT ''
+	,p_nradults INTEGER DEFAULT 0
+	,p_childbirthdate1 VARCHAR(10) DEFAULT ''
+	,p_childbirthdate2 VARCHAR(10) DEFAULT ''
+	,p_childbirthdate3 VARCHAR(10) DEFAULT ''
+	,p_childbirthdate4 VARCHAR(10) DEFAULT ''
+	)
+RETURNS TABLE (
+	TOCODE VARCHAR(20)
+	,MISCKEY VARCHAR(20)
+	,TOTAL DECIMAL(10, 2)
+	,DESTINATIONCODE VARCHAR(5)
+	,MISCCODE VARCHAR(30)
+	,TOURBOCODE VARCHAR(20)
+	,STATUS VARCHAR(4)
+	)
+
+NOT DETERMINISTIC LANGUAGE SQL
+
+BEGIN
+	ATOMIC
+
+	DECLARE startdate DATE;
+	DECLARE enddate DATE;
+	DECLARE currentdate DATE;
+	DECLARE childbirthdate1 DATE;
+	DECLARE childbirthdate2 DATE;
+	DECLARE childbirthdate3 DATE;
+	DECLARE childbirthdate4 DATE;
+
+	SET startdate = cast(nullif(p_startdate, '') AS DATE);
+	SET enddate = cast(nullif(p_enddate, '') AS DATE);
+	SET currentdate = coalesce(cast(nullif(p_currentdate, '') AS DATE), CURRENT DATE);
+	SET childbirthdate1 = cast(nullif(p_childbirthdate1, '') AS DATE);
+	SET childbirthdate2 = cast(nullif(p_childbirthdate2, '') AS DATE);
+	SET childbirthdate3 = cast(nullif(p_childbirthdate3, '') AS DATE);
+	SET childbirthdate4 = cast(nullif(p_childbirthdate4, '') AS DATE);
+
+	RETURN
+
+	SELECT TOCODE
+		,MISCKEY
+		,TOTAL
+		,DESTINATIONCODE
+		,MISCCODE
+		,TOURBOCODE
+		,STATUS
+	FROM TABLE ( func_miscpriceav(p_tocode, startdate, enddate, currentdate, p_nradults, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4) )
+;
+END
+@
+
+DROP FUNCTION func_miscpriceav2 @
+
+-- The function func_miscpriceav2 imporves the performance of the search for misc availability and pricing dramatically.
+-- It alos improves the interface that can be used to search for either a group of products, e.g. by destination,
+-- or by a specific product.
+--
+-- Example usage:
+-- select MISCKEY,TOTAL,STATUS from TABLE ( func_miscpriceav2( '', 'BKK', '2017-05-01', '2017-05-01', 2, '', '', '', '', '', '',1, 0, 1, '','' ) );
+-- This searches for all Misc products in BKK, ignoring all products with either a status XX or those without a price.
+--
+-- select MISCKEY,TOTAL,STATUS from TABLE ( func_miscpriceav2( '', '', '2017-04-01', '2017-04-01', 2, '', '', '', '', '', '',0, 0, 0, '','TOUWAL12828' ) );
+-- This searches for a specific Misc product with the MISCKEY=TOUWAL12828. A result set is returned irrespective of the price or status.
+--
+-- If a search is for a specific product with a specific IN_MISCKEY, then it makes sense for the parameters
+-- IN_IGNORE_XX=0
+-- IN_IGNORE_RQ=0
+-- IN_IGNORE_PRICE0=0
+--
+-- Setting these parameters only really makes sense when searching for products by destination.
+--
+CREATE FUNCTION func_miscpriceav2 (
+	IN_TOCODE VARCHAR(5) DEFAULT ''
+	,IN_DESTINATIONCODE VARCHAR(5) DEFAULT ''
 	,IN_PRICEDATEFROM VARCHAR(10)
 	,IN_PRICEDATETO VARCHAR(10)
 	,IN_NRADULTS INTEGER DEFAULT 2
@@ -136,19 +257,6 @@ CREATE FUNCTION func_miscpriceav (
 	)
 RETURNS TABLE (
 	MISCKEY VARCHAR(20)
-	,INVTITLE VARCHAR(120)
-	,INVDETAIL VARCHAR(3600)
-	,ITINTITLE VARCHAR(120)
-	,ITINDETAIL VARCHAR(3600)
-	,REGION VARCHAR(50)
-	,SUBREGION VARCHAR(50)
-	,COUNTRY VARCHAR(50)
-	,COUNTRYCODE VARCHAR(50)
-	,DESTINATIONCODE VARCHAR(50)
-	,MISCCODE VARCHAR(10)
-	,MISCITEMCODE VARCHAR(10)
-	,MINIMUMPERSONS INTEGER
-	,MAXIMUMPERSONS INTEGER
 	,TOTAL FLOAT
 	,STATUS VARCHAR(2)
 	) NOT DETERMINISTIC LANGUAGE SQL
@@ -217,58 +325,17 @@ BEGIN
 		,tmptable3(MISCKEY, STATUS, TOTAL) AS (
 			SELECT max(x.MISCKEY) AS MISCKEY
 				,max(x.STATUS) AS STATUS
-				,sum(cast(pricing.TOTAL AS FLOAT)) as TOTAL
+				,sum(cast(pricing.TOTAL AS FLOAT)) AS TOTAL
 			FROM tmptable2 x
 				,TABLE (func_pricing2_tbl(IN_TOCODE, x.MISCKEY, 'M', startdate, x.PRICEENDDATE, currentdate, x.NRADULTS, x.CHDDOB1, x.CHDDOB2, x.CHDDOB3, x.CHDDOB4)) AS pricing
 			GROUP BY x.MISCKEY
 			)
 
 	SELECT TOOMISC.MISCKEY AS MISCKEY
-		,INV.TITLE AS INVTITLE
-		,INV.DETAIL AS INVDETAIL
-		,ITIN.TITLE AS ITINTITLE
-		,ITIN.DETAIL AS ITINDETAIL
-		,TOOMISC.REGION AS REGION
-		,TOOMISC.SUBREGION AS SUBREGION
-		,TOOMISC.COUNTRY AS COUNTRY
-		,TOOMISC.COUNTRYISOCODE AS COUNTRYCODE
-		,TOOMISC.DESTINATIONCODE AS DESTINATIONCODE
-		,TOOMISC.MISCCODE AS MISCCODE
-		,TOOMISC.MISCITEMCODE AS MISCITEMCODE
-		,TOOMISC.MINIMUMPERSONS AS MINIMUMPERSONS
-		,TOOMISC.MAXIMUMPERSONS AS MAXIMUMPERSONS
 		,coalesce(x.TOTAL, 0) AS TOTAL
 		,coalesce(x.STATUS, 'XX') AS STATUS
 	FROM TOOMISC
 	LEFT OUTER JOIN tmptable3 x ON x.MISCKEY = TOOMISC.MISCKEY
-	LEFT OUTER JOIN TOOMISCTEXT AS INV ON TOOMISC.MISCKEY = INV.MISCKEY
-		AND TOOMISC.TOCODE = INV.TOCODE
-		AND INV.type = 'INV'
-		AND INV.LANG = (
-			CASE upper(IN_LANGCODE)
-				WHEN 'EN'
-					THEN 'EN'
-				WHEN 'FR'
-					THEN 'FR'
-				WHEN 'IT'
-					THEN 'IT'
-				ELSE 'DE'
-				END
-			)
-	LEFT OUTER JOIN TOOMISCTEXT AS ITIN ON TOOMISC.MISCKEY = ITIN.MISCKEY
-		AND TOOMISC.TOCODE = ITIN.TOCODE
-		AND ITIN.type = 'ITIN'
-		AND ITIN.LANG = (
-			CASE upper(IN_LANGCODE)
-				WHEN 'EN'
-					THEN 'EN'
-				WHEN 'FR'
-					THEN 'FR'
-				WHEN 'IT'
-					THEN 'IT'
-				ELSE 'DE'
-				END
-			)
 	WHERE TOOMISC.DESTINATIONCODE = coalesce(nullif(IN_DESTINATIONCODE, ''), TOOMISC.DESTINATIONCODE)
 		AND TOOMISC.MISCCODE = coalesce(nullif(IN_MISCCODE, ''), TOOMISC.MISCCODE)
 		AND TOOMISC.MISCITEMCODE = coalesce(nullif(IN_MISCITEMCODE, ''), TOOMISC.MISCITEMCODE)
@@ -296,65 +363,3 @@ BEGIN
 			);
 END
 @
-
-DROP FUNCTION func_miscpriceav_ch @
-
--- select * from TABLE( func_miscpriceav_ch ('', '2015-11-12', '2015-11-12', '', 1 , '', '', '', '') )
--- select * from TABLE( func_miscpriceav_ch ('TSOL', '2015-11-12', '2015-11-12', '', 1 , '', '', '', '') )
-CREATE FUNCTION func_miscpriceav_ch (
-	p_tocode VARCHAR(5) DEFAULT ''
-	,p_startdate VARCHAR(10) DEFAULT ''
-	,p_enddate VARCHAR(10) DEFAULT ''
-	,p_currentdate VARCHAR(10) DEFAULT ''
-	,p_nradults INTEGER DEFAULT 0
-	,p_childbirthdate1 VARCHAR(10) DEFAULT ''
-	,p_childbirthdate2 VARCHAR(10) DEFAULT ''
-	,p_childbirthdate3 VARCHAR(10) DEFAULT ''
-	,p_childbirthdate4 VARCHAR(10) DEFAULT ''
-	)
-RETURNS TABLE (
-	TOCODE VARCHAR(20)
-	,MISCKEY VARCHAR(20)
-	,TOTAL DECIMAL(10, 2)
-	,DESTINATIONCODE VARCHAR(5)
-	,MISCCODE VARCHAR(30)
-	,TOURBOCODE VARCHAR(20)
-	,STATUS VARCHAR(4)
-	)
-
-NOT DETERMINISTIC LANGUAGE SQL
-
-BEGIN
-	ATOMIC
-
-	DECLARE startdate DATE;
-	DECLARE enddate DATE;
-	DECLARE currentdate DATE;
-	DECLARE childbirthdate1 DATE;
-	DECLARE childbirthdate2 DATE;
-	DECLARE childbirthdate3 DATE;
-	DECLARE childbirthdate4 DATE;
-
-	SET startdate = cast(nullif(p_startdate, '') AS DATE);
-	SET enddate = cast(nullif(p_enddate, '') AS DATE);
-	SET currentdate = coalesce(cast(nullif(p_currentdate, '') AS DATE), CURRENT DATE);
-	SET childbirthdate1 = cast(nullif(p_childbirthdate1, '') AS DATE);
-	SET childbirthdate2 = cast(nullif(p_childbirthdate2, '') AS DATE);
-	SET childbirthdate3 = cast(nullif(p_childbirthdate3, '') AS DATE);
-	SET childbirthdate4 = cast(nullif(p_childbirthdate4, '') AS DATE);
-
-	RETURN
-
-	SELECT TOCODE
-		,MISCKEY
-		,TOTAL
-		,DESTINATIONCODE
-		,MISCCODE
-		,TOURBOCODE
-		,STATUS
-	FROM TABLE ( func_miscpriceav(p_tocode, startdate, enddate, currentdate, p_nradults, childbirthdate1, childbirthdate2, childbirthdate3, childbirthdate4) )
-;
-END
-@
-
-
